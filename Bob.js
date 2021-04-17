ac081f2b16f527684494ff15dc5cab16375fb1d6
Aryansharma906 <76518533+Aryansharma906@users.noreply.github.com>
2021-04-17 22:17:41 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,25 @@
+class Bob {
+    constructor(x, y, diameter) {
+      var options = {
+            isStatic: false,
+            restitution: 1.2,
+            friction: 0.4,
+            density: 1.5
+      }
+    this.body = Bodies.circle(x, y, diameter/2, options);
+    this.diameter = diameter;
+
+      
+    World.add(world, this.body);
+    }
+    display(){
+       this.body.shapeColor = "blue"
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        fill("red");
+        ellipse(0, 0, this.diameter);
+        pop();
+    }
+  };
